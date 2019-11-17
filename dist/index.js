d6f906e4a3c80fbacae0cb86f753bd88ff302700
Venryx <venryx@gmail.com>
2019-11-17 04:17:44 -0800

    * Added more params for "E" function.

--- dist/index.js
+++ dist/index.js
@@ -1626,7 +1626,7 @@ exports.FromJSON = FromJSON;
 exports.AsMultiline = AsMultiline;
 exports.Assert = Assert;
 exports.WrapWithGo = WrapWithGo;
-function E(e1, e2, e3, e4, e5, e6, e7, e8) {
+function E(e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12, e13, e14, e15, e16, e17, e18, e19, e20) {
     var result = {};
     var _iteratorNormalCompletion = true;
     var _didIteratorError = false;
